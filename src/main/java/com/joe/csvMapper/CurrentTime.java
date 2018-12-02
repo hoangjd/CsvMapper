5cff78f1834bd13ad7bf472d21142eccf035ab98
Joe <hoangjd@vcu.edu>
2018-12-02 14:24:25 -0500

    edited date formatting

--- src/main/java/com/joe/csvMapper/CurrentTime.java
+++ src/main/java/com/joe/csvMapper/CurrentTime.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class CurrentTime {
 
     public String getCurrentTime() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
         Date date = new Date();
         return formatter.format(date);
     }
